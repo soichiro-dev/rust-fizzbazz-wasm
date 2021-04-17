126b403d32ff0ac3f14ee6d7986445d0e057be3f
soichiro-dev <soichiro-dev@gmail.com>
2021-04-18 00:27:18 +0900

    fizzbuzz結果を警告ダイアログで表示するよう修正

--- sample/index.js
+++ sample/index.js
@@ -1,4 +1,4 @@
 const js = import("./node_modules/@soichiro_dev/rust-fizzbazz-wasm/rust_fizzbazz_wasm.js");
 js.then(js => {
-  js.greet("WebAssembly");
+  js.greet();
 });
--- src/lib.rs
+++ src/lib.rs
@@ -7,12 +7,28 @@ use wasm_bindgen::prelude::*;
 // JavaScript 内の外部関数を Rust から呼び出す
 #[wasm_bindgen]
 extern {
-    pub fn alert(s: &str);
+    pub fn alert(s: String);
 }
 
 
 // JavaScript が呼び出せる Rust 関数の作成
 #[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
+// TODO: 関数名が挨拶では動作がわからない
+pub fn greet() {
+    // TODO: 文字列の連結をする必要があるか？配列など結果だけ返せばよいのでは
+    let mut result: String = "".to_string();
+    for n in 1..101 {
+        if n % 15 == 0 {
+            result.push_str("fizzbuzz");
+        } else if n % 3 == 0 {
+            result.push_str("fizz");
+        } else if n % 5 == 0 {
+            result.push_str("buzz");
+        } else {
+            result.push_str(&n.to_string());
+        }
+    }
+
+    // TODO: 表示は呼び出し元で行ったほうが良い
+    alert(result);
 }
