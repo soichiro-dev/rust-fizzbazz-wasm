e096b8daa21b946648f32a3d9643d92b7c18c1ef
soichiro-dev <soichiro-dev@gmail.com>
2021-04-18 07:47:52 +0900

    ダイアログではなく画面内に結果を表示するように修正

--- sample/index.js
+++ sample/index.js
@@ -1,4 +1,8 @@
-const js = import("./node_modules/@soichiro_dev/rust-fizzbazz-wasm/rust_fizzbazz_wasm.js");
-js.then(js => {
-  js.greet();
-});
+// TODO: wasmファイルをimportするようにできないか
+// npm installしたモジュールをimport
+const fizzBuzzModule = import("./node_modules/@soichiro_dev/rust-fizzbazz-wasm/rust_fizzbazz_wasm.js");
+
+// モジュール内の関数を実行しFizzBuzzゲームの結果を表示
+fizzBuzzModule
+.then(module => module.greet())
+.then(result => document.write(result));
--- src/lib.rs
+++ src/lib.rs
@@ -4,31 +4,24 @@ extern crate wasm_bindgen;
 use wasm_bindgen::prelude::*;
 
 
-// JavaScript 内の外部関数を Rust から呼び出す
-#[wasm_bindgen]
-extern {
-    pub fn alert(s: String);
-}
-
-
 // JavaScript が呼び出せる Rust 関数の作成
 #[wasm_bindgen]
 // TODO: 関数名が挨拶では動作がわからない
-pub fn greet() {
+pub fn greet() -> String {
     // TODO: 文字列の連結をする必要があるか？配列など結果だけ返せばよいのでは
     let mut result: String = "".to_string();
     for n in 1..101 {
         if n % 15 == 0 {
-            result.push_str("fizzbuzz");
+            result.push_str("FizzBuzz");
         } else if n % 3 == 0 {
-            result.push_str("fizz");
+            result.push_str("Fizz");
         } else if n % 5 == 0 {
-            result.push_str("buzz");
+            result.push_str("Buzz");
         } else {
             result.push_str(&n.to_string());
         }
+        result.push_str("\n");
     }
 
-    // TODO: 表示は呼び出し元で行ったほうが良い
-    alert(result);
+    return result;
 }
